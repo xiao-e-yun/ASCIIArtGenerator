91611c38b6b30252bd2ca8d26ae17dd20b3cb6c6
xiao-e-yun <a0970323059@gmail.com>
2025-05-17 16:41:36 +0800

    format

--- src/composables/text_renderer.ts
+++ src/composables/text_renderer.ts
@@ -1,8 +1,8 @@
-import {computed, ref, toRaw, toValue, type MaybeRefOrGetter} from 'vue'
-import {watchDeep} from '@vueuse/core'
+import { computed, ref, toRaw, toValue, type MaybeRefOrGetter } from 'vue'
+import { watchDeep } from '@vueuse/core'
 
 import TextWorker from './text_renderer.worker?worker'
-import {chain, cloneDeep, isEqual} from 'lodash'
+import { chain, cloneDeep, isEqual } from 'lodash'
 
 export type TexTRenderingRequest = {
   granularity: [number, number]
@@ -20,39 +20,42 @@ export const useTextRenderer = (
 
   const worker = new TextWorker()
 
-  watchDeep([characters, granularity] as const, ([characters, granularity]) => {
-    if (!isEqual(toValue(granularity), prevGranularity.value)) {
-      charactersData.value = {} as Record<string, number[]>
-      prevGranularity.value = cloneDeep(toValue(granularity))
-    }
+  watchDeep(
+    [characters, granularity] as const,
+    ([characters, granularity]) => {
+      if (!isEqual(toValue(granularity), prevGranularity.value)) {
+        charactersData.value = {} as Record<string, number[]>
+        prevGranularity.value = cloneDeep(toValue(granularity))
+      }
 
-    const charactersSet = toValue(characters)
-      .split('')
-      .filter((c) => {
-        if (c === '\n') return false
-        if (charactersData.value[c] !== undefined) return false
-        charactersData.value[c] = null
-        return true
-      })
+      const charactersSet = toValue(characters)
+        .split('')
+        .filter((c) => {
+          if (c === '\n') return false
+          if (charactersData.value[c] !== undefined) return false
+          charactersData.value[c] = null
+          return true
+        })
 
-    worker.postMessage({
-      characters: charactersSet,
-      granularity: toRaw(toValue(granularity)),
-    } as TexTRenderingRequest)
-  }, { immediate: true })
+      worker.postMessage({
+        characters: charactersSet,
+        granularity: toRaw(toValue(granularity)),
+      } as TexTRenderingRequest)
+    },
+    { immediate: true },
+  )
 
-  worker.onmessage = ({data}: MessageEvent<TexTRenderingResult>) => {
+  worker.onmessage = ({ data }: MessageEvent<TexTRenderingResult>) => {
     for (const [char, pixels] of data) {
       charactersData.value[char] = pixels
     }
   }
 
-  const charactersPixels = computed(
-    () =>
-      chain(toValue(characters).split(''))
-        .filter(c => c !== '\n')
-        .map((c) => [c, charactersData.value[c]] as [string, number[] | null])
-        .value()
+  const charactersPixels = computed(() =>
+    chain(toValue(characters).split(''))
+      .filter((c) => c !== '\n')
+      .map((c) => [c, charactersData.value[c]] as [string, number[] | null])
+      .value(),
   )
 
   return {
