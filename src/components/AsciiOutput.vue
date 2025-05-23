544aabfae73fcdad455962b06e13f04d2deea19a
xiao-e-yun <a0970323059@gmail.com>
2025-05-24 02:35:00 +0800

    remove click to select all ascii

--- src/components/AsciiOutput.vue
+++ src/components/AsciiOutput.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { DisplayMode } from '@/utils'
-import { useElementSize } from '@vueuse/core'
-import { computed, useTemplateRef } from 'vue'
+import {DisplayMode} from '@/utils'
+import {useElementSize} from '@vueuse/core'
+import {computed, useTemplateRef} from 'vue'
 
 const TEXT_ASPECT_RATIO = 0.55
 
@@ -12,41 +12,19 @@ const props = defineProps<{
 }>()
 
 const el = useTemplateRef('el')
-const { width } = useElementSize(el)
+const {width} = useElementSize(el)
 
 const fontSize = computed(() => {
   const widthSize =
     width.value / props.size[0] / (props.mode === DisplayMode.Square ? 1 : TEXT_ASPECT_RATIO)
   return widthSize
 })
-
-const select = (event: MouseEvent) => {
-  const target = event.target as HTMLPreElement
-  const selection = window.getSelection()
-  if (!selection) return
-
-  // check if already selected
-  if (selection.toString().includes(target.innerText)) {
-    selection.removeAllRanges()
-    return
-  }
-
-  const range = document.createRange()
-  range.selectNodeContents(target)
-  selection.removeAllRanges()
-  selection.addRange(range)
-}
 </script>
 
 <template>
   <div ref="el">
-    <pre
-      class="origin-left bg-white text-black w-fit leading-none font-mono"
-      :style="{
-        fontSize: fontSize.toString() + 'px',
-      }"
-      v-text="text"
-      @click="select"
-    />
+    <pre class="origin-left bg-white text-black w-fit leading-none font-mono" :style="{
+      fontSize: fontSize.toString() + 'px',
+    }" v-text="text" />
   </div>
 </template>
